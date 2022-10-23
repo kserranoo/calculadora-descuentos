a08d975ad7bca72f8ab1b31b87f50faf492a29d2
Kimberlyn Serrano <113371632+kserranoo@users.noreply.github.com>
2022-10-23 19:24:17 -0400

    Cupones de descuento
    Se realizó la lógica para la aplicación de cupones.
    
    Si el usuario tipea un código de cupón válido, se le aplica el descuento a su compra, en caso contrario, no se aplica ningún descuento y se envía un mensaje para que el usuario verifique los datos e intente nuevamente.
    
    También un poco de css.

--- main.js
+++ main.js
@@ -1,25 +1,58 @@
 const inputPrecio = document.getElementsByClassName('precio')[0];
-const inputDescuento = document.getElementsByClassName('descuento')[0];
+const inputCoupon = document.getElementsByClassName('coupon')[0];
 const oferta = document.getElementsByClassName('oferta')[0];
 const btnCalcular = document.getElementsByClassName('btn-calcular')[0];
 const btnReset = document.getElementsByClassName('btn-reset')[0];
     
-btnCalcular.addEventListener('click', calcularDescuento);
+btnCalcular.addEventListener('click', aplicarCupon);
 btnReset.addEventListener('click', _ => {location.reload()});
 
-    function calcularDescuento() {
+const couponList = [];
+couponList.push({
+    name: 'regreso-a-clases',
+    discount: 30,
+})
+couponList.push({
+    name: 'feliz-cumple',
+    discount: 25,
+})
+couponList.push({
+    name: 'empieza-la-navidad',
+    discount: 20,
+})
+couponList.push({
+    name: 'bienvenido-al-club',
+    discount: 15,
+})
+couponList.push({
+    name: 'pruebame',
+    discount: 90,
+})
+
+    function aplicarCupon() {
 
         const price = Number(inputPrecio.value);
-        const discount = Number(inputDescuento.value);
+        const coupon = inputCoupon.value;
         
 
-        if (!price || !discount) {
+        if (!price || !coupon) {
             oferta.innerText = "Por favor, ingrese los datos de su compra";
-           return
+           return;
         }
-        if (discount> 100) {
-            oferta.innerText = "El % de su cupón no puede ser mayor a 100";
-           return  
+
+        let discount;
+
+        function validCoupon (couponElement) {
+            return couponElement.name == coupon;
+        }
+
+       const isCouponValid = couponList.find(validCoupon);
+
+        if (isCouponValid) {
+            discount = isCouponValid.discount
+        } else {
+            oferta.innerText = "Su cupón no es válido."
+            return;
         }
 
         const newPrice = (price*(100-discount))/100;
